e17464c57d50d815b632dc5bdb26a87ef668a59c
meepawn2019 <meepawn2018@gmail.com>
2022-09-20 16:37:50 +0700

    add play button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import data from "./csvjson.json";
@@ -10,117 +10,162 @@ import {
   Legend,
 } from "chart.js";
 import { Bubble } from "react-chartjs-2";
-import { END_DATE, START_DATE, transformData } from "./utils/utils";
+import { END_DATE, formatter, FORMAT_PROP, numFormatter, POPULATION_LABEL, START_DATE, transformData } from "./utils/utils";
+import useInterval from "./hooks/useInterval";
+const ONE_MILLION = 1_000_000;
 
 ChartJS.register(LinearScale, PointElement, Tooltip, Legend);
 
-const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  animation: false,
-  scales: {
-    yAxis: {
-      title: {
-        text: "Fetility",
-        display: true,
-      },
-      max: 9,
-      min: 0.5,
-    },
-    x: {
-      title: {
-        text: "Life Expectancy",
-        display: true,
-      },
-      max: 90,
-      min: 50,
-    },
-  },
-  plugins: {
-    tooltip: {
-      callbacks: {
-        label: (context) => {
-          return context.dataset.label;  
-        }
-      }
-    },
-    legend: {
-      display: true,
-    }
-  }
-};
 
 function App() {
-  const testData = transformData(data, 0, 20);
+  const {obj, chartObj: testData} = transformData(data, 0, 20);
   const [sliderValue, setSliderValue] = useState(START_DATE);
   const [sliderMax, setSliderMax] = useState(20);
   const [chartData, setChartData] = useState(testData);
+  const [autoIncrement, setAutoIncrement] = useState(null);
+  const [population, setPopulation] = useState(0);
+  const [fertility, setFertility] = useState(0);
+  const [lifeExpectancy, setLifeExpectancy] = useState(0);
+  const yearSliderRef = useRef(null);
 
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    animation: false,
+    scales: {
+      yAxis: {
+        title: {
+          text: "Fetility",
+          display: true,
+        },
+        max: 9,
+        min: 0.5,
+      },
+      x: {
+        title: {
+          text: "Life Expectancy",
+          display: true,
+        },
+        max: 90,
+        min: 50,
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context) => {
+            return context.dataset.label;
+          },
+        },
+      },
+    },
+    onClick: (e, elements) => {
+      const index = elements[0].datasetIndex;
+      const dataIndex = elements[0].index;
+      const datasetLabel = e.chart.data.datasets[index].label;
+      const value = e.chart.data.datasets[index].data[dataIndex];
+      setPopulation(value.r < sliderMax ? value.r * ONE_MILLION : obj[datasetLabel][POPULATION_LABEL][FORMAT_PROP.replace(/{{year}}/g, sliderValue)]);
+      setFertility(value.y);
+      setLifeExpectancy(value.x);
+    }
+  };
+
+  const increaseYears = () => {
+    const newValue = parseInt(sliderValue) + 1;
+    if (newValue > END_DATE) {
+      setAutoIncrement(null);
+      return;
+    };
+    const { chartObj: newData} = transformData(data, newValue - START_DATE, sliderMax);
+    setSliderValue(newValue);
+    setChartData(newData);
+  };
+
+  useInterval(increaseYears, autoIncrement);
+
+  const handleAutoIncrement = () => {
+    if (autoIncrement) {
+      setAutoIncrement(null);
+    } else {
+      setAutoIncrement(400);
+    }
+  };
 
   const handleChangeYears = useCallback(
     (e) => {
       const years = e.target.value;
-      console.log(years);
       setSliderValue(years);
-      const newData = transformData(data, years - START_DATE, sliderMax);
+      const { chartObj: newData } = transformData(data, years - START_DATE, sliderMax);
       setChartData(newData);
     },
-    [sliderValue, chartData, sliderMax]
+    [sliderValue, chartData]
   );
 
   const handleChangeMax = useCallback(
     (e) => {
       const max = e.target.value;
       setSliderMax(max);
-      const newData = transformData(data, sliderValue - START_DATE, max);
+      const { chartObj: newData } = transformData(data, sliderValue - START_DATE, max);
       setChartData(newData);
     },
-    [sliderValue, chartData, sliderMax]
+    [chartData, sliderMax]
   );
 
   return (
     <div className="App">
+      <p className="text-left">{`Population: ${numFormatter.format(population)}`}</p>
+      <p className="text-left">{`Fetility: ${formatter.format(fertility)}`}</p>
+      <p className="text-left">{`Life expectancy: ${formatter.format(lifeExpectancy)}`}</p>
       <div className="wrapper">
         {<Bubble options={options} data={chartData} />}
-        <input
-          type={"range"}
-          min={START_DATE}
-          max={END_DATE}
-          style={{
-            width: "100%",
-            height: "20px",
-          }}
-          defaultValue={sliderValue}
-          onChange={handleChangeYears}
-        />
-        <div
-          style={{
-            position: "relative",
-            width: "100%",
-            left: `${(sliderValue - START_DATE) / (END_DATE - START_DATE)}px`,
-          }}
-        >
-          <p
-            style={{
-              position: "absolute",
-              left: `calc(${
-                ((sliderValue - START_DATE) / (END_DATE - START_DATE)) * 1000
-              }px - 18px)`,
-              marginTop: 0,
-              marginBottom: 0,
-            }}
-          >
-            {sliderValue}
-          </p>
+        <div  className="year-input">
+          <button style={{marginRight: "12px"}} onClick={handleAutoIncrement}>TEST</button>
+          <div style={{ flexGrow: 1 }} ref={yearSliderRef}>
+            <input
+              type={"range"}
+              min={START_DATE}
+              max={END_DATE}
+              style={{
+                width: "100%",
+                height: "20px",
+              }}
+              defaultValue={sliderValue}
+              value={sliderValue}
+              onChange={handleChangeYears}
+            />
+            <div
+              style={{
+                position: "relative",
+                width: "100%",
+                left: `${
+                  (sliderValue - START_DATE) / (END_DATE - START_DATE)
+                }px`,
+              }}
+            >
+              <p
+                style={{
+                  position: "absolute",
+                  left: `calc(${
+                    ((sliderValue - START_DATE) / (END_DATE - START_DATE)) *
+                    yearSliderRef?.current?.offsetWidth
+                  }px - 18px)`,
+                  marginTop: 0,
+                  marginBottom: 0,
+                }}
+              >
+                {sliderValue}
+              </p>
+            </div>
+          </div>
         </div>
         <input
           type={"range"}
           min={2}
-          max={87}
+          max={150}
           style={{
             width: "100%",
             height: "20px",
-            marginTop: "50px"
+            marginTop: "50px",
           }}
           defaultValue={sliderMax}
           onChange={handleChangeMax}
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,26 +1,41 @@
-const LIFE_EXPECTANCY_LABEL = "Life expectancy at birth, total (years)";
-const ACCESS_TO_ELECTRICITY_LABEL = "Fertility rate, total (births per woman)";
-const POPULATION_LABEL = "Population ages 15-64, total";
+export const LIFE_EXPECTANCY_LABEL = "Life expectancy at birth, total (years)";
+export const ACCESS_TO_ELECTRICITY_LABEL = "Fertility rate, total (births per woman)";
+export const POPULATION_LABEL = "Population ages 15-64, total";
 export const START_DATE = 1997;
 export const END_DATE = 2020;
-const FORMAT_PROP = "{{year}} [YR{{year}}]";
+export const FORMAT_PROP = "{{year}} [YR{{year}}]";
 
 const BACKGROUND_COLOR_MAP = {
   Vietnam: "rgba(255, 99, 132)",
   "Hong Kong SAR, China": "rgba(54, 162, 235)",
   "Macao SAR, China": "rgba(255, 206, 86)",
   "Korea, Rep.": "rgba(75, 192, 192)",
-  "Korea, Dem. People’s Rep.": "rgba(153, 102, 255)",
+  "Korea, Dem. People's Rep.": "rgba(153, 102, 255)",
   Japan: "rgba(255, 159, 64)",
   Singapore: "rgba(255, 99, 132)",
+  Italy: "rgba(54, 162, 235)",
+  India: "rgba(255, 206, 86)",
+  "Russian Federation": "rgba(75, 192, 192)",
+  Australia: "rgba(153, 102, 255)",
+  Canada: "rgba(255, 159, 64)",
+  China: "rgba(255, 99, 132)",
+  Jordan: "rgba(54, 162, 235)",
+
 };
 
-export const generateRandomBackground = () => {
-  // generate random color
-};
+export const formatter = new Intl.NumberFormat('en-US', {
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 4,
+  minimumSignificantDigits: 1,
+  maximumSignificantDigits: 4
+})
+
+export const numFormatter = new Intl.NumberFormat('en-GB', {
+  notation: "compact",
+  compactDisplay: "short",
+})
 
 export const transformData = (data, addedYears = 0, maxRadius) => {
-  console.log(data);
   const testData = data.filter((e) => e["Country Name"] && e["Country Code"]);
   const obj = {};
   for (const entry of testData) {
@@ -70,5 +85,5 @@ export const transformData = (data, addedYears = 0, maxRadius) => {
   chartObj.datasets.sort((a, b) => {
     return a.data[0].r - b.data[0].r;
   });
-  return chartObj;
+  return {obj, chartObj};
 };
